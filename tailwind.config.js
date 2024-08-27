3665f37db3b29bbaa6f11903ba90ca850f5b92d2
samia053 <samiashameemhaque@gmail.com>
2024-08-27 23:11:14 +0600

    added all the styles except navbar responsivness

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        backgroundImage: {
+          'bg-img': "url('assignment03/assets/bg.jpg')",
+          
+        }
+      },
+    },
+    plugins: [],
+  }
